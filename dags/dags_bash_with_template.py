fe6ffef3454a0f0fcb01724be8ee4e01c30129c3
calebsuminkim <hpsm5187@gmail.com>
2024-03-06 15:33:17 +0900

    bash_tamplate_dag업로드

---
+++ dags/dags_bash_with_template.py
@@ -0,0 +1,25 @@
+import datetime
+import pendulum
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id='dag_bash_with_template',
+    schedule='10 0 * * *',
+    start_date=pendulum.datetime(2023, 3, 1, tz='Asia/Seoul'),
+    catchup=False
+) as dag:
+    bash_t1 = BashOperator(
+        task_id='bash_t1',
+        bash_command='echo "data_interval_end : {{ data_interval_end }}"'
+    )
+    bash_t2 = BashOperator(
+        task_id='bash_t2',
+        env={
+            'START_DATE' : '{{data_interval_start | ds}}',
+            'END_DATE' : '{{data_intercal_end | ds}}'
+        },
+        bash_command='echo $START_DATE && echo $END_DATE' # echo A && B : A커맨드가 성공하면 B커맨드 실행
+    )
+
+    bash_t1 >> bash_t2
