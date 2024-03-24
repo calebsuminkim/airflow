936d3266cc73211e949c6fd1b9524ab233eb49ea
calebsuminkim <hpsm5187@gmail.com>
2024-03-24 22:31:42 +0900

    simplehttpoperator실습 파일 업로드 : 서울시 인재 정보 가져오기

---
+++ dags/dags_simple_http_operator.py
@@ -0,0 +1,37 @@
+import pendulum
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.decorators import task
+
+with DAG(
+    dag_id='dags_simple_http_operator',
+    start_date=pendulum.datetime(2024, 3, 1, tz='Asia/Seoul'),
+    schedule=None,
+    catchup=False
+) as dag:
+    
+    '''서울시 공공데이터 정보'''
+    # http://openapi.seoul.go.kr:8088/(인증키)/xml/TnJbhntBassOpen/1/5/
+    get_hr_data = SimpleHttpOperator(
+        task_id='get_hr_data',
+        http_conn_id='openapi.seoul.go.kr',
+        endpoint='{{var.value.apikey_openapi_seoul_go_kr}}/xml/TnJbhntBassOpen/1/10/',
+        method='GET',
+        headers={
+            'Content-Type':'application/json',
+            'charset':'utf-8',
+            'Accept':'*/*'
+        }
+    )
+
+    @task(task_id='python_2')
+    def python_2(**kwargs):
+        ti = kwargs['ti']
+        rslt = ti.xcom_pull(task_ids='get_hr_data') # SimpleHttpOperator가 가진 데이터를 가져오기
+        import xml
+        from pprint import pprint
+
+        pprint(xml.loads(rslt))
+
+    get_hr_data >> python_2()
