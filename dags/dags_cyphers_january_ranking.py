90d84a8d06590ed477fb519e95ff7152c9a9a27e
calebsuminkim <hpsm5187@gmail.com>
2024-03-30 22:54:28 +0900

    파라미터 수정; Chrome(driver_path, options=options)

--- dags/dags_cyphers_january_ranking.py
+++ dags/dags_cyphers_january_ranking.py
@@ -26,9 +26,9 @@ with DAG(
         options = webdriver.ChromeOptions()
         options.add_argument('headless')
 
-        # driver_path = '/usr/local/bin/'
+        driver_path = '/usr/local/bin/'
 
-        driver = webdriver.Chrome(options = options)
+        driver = webdriver.Chrome(driver_path, options = options)
         # driver = webdriver.Chrome(executable_path = driver_path, options = options)
         
         url = 'https://cyphers.nexon.com/ranking/character/january'
