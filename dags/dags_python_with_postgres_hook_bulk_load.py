5b8e14ec20df09d4b3dc1b0ced24d792d0c730eb
calebsuminkim <hpsm5187@gmail.com>
2024-03-31 18:33:51 +0900

    데이터를 지진발생 데이터로 변경

--- dags/dags_python_with_postgres_hook_bulk_load.py
+++ dags/dags_python_with_postgres_hook_bulk_load.py
@@ -18,7 +18,7 @@ with DAG(
         python_callable=insrt_postgres,
         op_kwargs={
             'postgres_conn_id' : 'conn-db-postgres-custom',
-            'tbl_nm' : 'airPolutionMeasuring1Hour_bulk',
-            'file_nm' : '/opt/airflow/files/airPolutionMeasuring1Hour_bulk/{{ data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}/airPolutionMeasuring1Hour_bulk.csv'
+            'tbl_nm' : 'TbEqkKenvinfo_bulk',
+            'file_nm' : '/opt/airflow/files/TbEqkKenvinfo_bulk/{{ data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}/TbEqkKenvinfo_bulk.csv'
         }
     )
