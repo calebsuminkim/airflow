71222ba18a75d7dc286b1a46669d777ad0e513c3
calebsuminkim <hpsm5187@gmail.com>
2024-05-07 20:47:32 +0900

    catchup = False주는거 까먹음 -> 추가됨.

--- dags/dags_email_on_failure.py
+++ dags/dags_email_on_failure.py
@@ -12,6 +12,7 @@ email_lst = [email.strip() for email in email_str.split(',')]
 with DAG(
     dag_id='dags_email_on_failure',
     start_date=pendulum.datetime(2024, 5, 1, tz='Asia/Seoul'),
+    catchup=False,
     schedule='0 1 * * *',
     dagrun_timeout=timedelta(minutes=2),
     default_args={
